fc4b7f799e886bab5a6d741eb540744e6172b820
Arkadiusz Plumbaum <arekpl28@wp.pl>
2021-07-29 20:35:03 +0200

    Add task

---
+++ aplikacja.py
@@ -0,0 +1,41 @@
+from zadanie import Zadanie
+
+
+class Aplikacja:
+    def __init__(self):
+        self.zadania = [
+            Zadanie('Posprzątaj pokój'),
+            Zadanie('Umyj auto')
+        ]
+
+    def wypisz(self):
+        for indeks, zadanie in enumerate(self.zadania):
+            print(f'[{indeks}].', zadanie.wypisz())
+
+    def dodaj(self):
+        nazwa = self.pobierz_nazwe_przypomnienia()
+        zadanie = Zadanie(nazwa)
+        self.zadania.append(zadanie)
+        self.wypisz()
+
+    def oznacz_jako_zrobione(self):
+        indeks = self.pobierz_indeks()
+        self.zadania[indeks].ustaw_jako_zrobione()
+        self.wypisz()
+
+    def pobierz_indeks(self) -> int:
+        jest_ok = False
+        while not jest_ok:
+            indeks = int(
+                input("Które wydarzenie mam oznaczyć jako zrobione: "))
+            if indeks < len(self.zadania):
+                jest_ok = True
+        return indeks
+
+    def pobierz_nazwe_przypomnienia(self) -> str:
+        jest_ok = False
+        while not jest_ok:
+            nazwa = input('Powiedz, co masz do zrobienia? ')
+            if len(nazwa) > 3:
+                jest_ok = True
+        return nazwa
---
+++ main.py
@@ -0,0 +1,21 @@
+from aplikacja import Aplikacja
+
+
+if __name__ == '__main__':
+    aplikacja = Aplikacja()
+    aplikacja.wypisz()
+    while True:
+        print('-' * 30)
+        print(
+            'Co chcesz zrowbić? [w-wypisz, d-dodaj, z-oznacz jako zrobione, q-zakończ]: ')
+        polecenie = input('Napisz "w", "d", "z" lub "q" : ')
+        if polecenie == "w":
+            aplikacja.wypisz()
+        elif polecenie == "d":
+            aplikacja.dodaj()
+        elif polecenie == "z":
+            aplikacja.oznacz_jako_zrobione()
+        elif polecenie == "q":
+            quit()
+        else:
+            print("Nie rozumiem...")
